6a829de5f2568b57714c27d8222bfde765dc0c78
Christoph Forster <christoph.forster@gmail.com>
2015-11-03 00:51:11 -0800

    Collection Examples

--- js/bsp_07.js
+++ js/bsp_07.js
@@ -18,4 +18,34 @@
             border: "2px solid green",
             backgroundColor : "#aaaaff" // Alternativ dazu könnte man auch "background-color" schreiben
         });
+
+    // css selector nth-child - jedes 2. Element, startet mit 4. Element
+    $("li:nth-child(2n+4)").css({
+        fontWeight : "bold"
+    });
+    //jQuery Selektor (schlechtere Performance)
+    //$("li:even").css({
+    //   backgroundColor:"green"
+    //});
+
+    //besser
+    $("li").filter(":even").css({
+        backgroundColor:"green"
+    });
+
+    // $("li:first-child") - css selektor
+    $("li:first").css({
+        color:"white"
+    });
+
+    // Text contains case sensitive "D"
+    $("li:contains('D')").css({
+        backgroundColor:"pink"
+    });
+
+    // $("h3").parent().css("border", "2px solid blue");
+
+    $("article").filter(":has(h3)").css("border", "2px solid blue");
+
+
 }(jQuery));

55c9177a6644edee228e756d2cf62dc25d939929
Christoph Forster <christoph.forster@gmail.com>
2015-11-03 00:23:23 -0800

    Extended Collection - added more Content to html

--- js/bsp_07.js
+++ js/bsp_07.js
@@ -3,6 +3,19 @@
  */
 (function ($) {
     "use strict";
+
+    //Collection enthält alle h2
     $("h2").text("Alles Neu macht der Mai");
-    $("h2").add("div").css("backgroundColor", "yellow");
+
+    // der h2 COllection werden alle p-Elemente hinzugefügt
+    $("h2")
+        .add("div")
+        .css("backgroundColor", "yellow")
+
+        // FIlter - erstes Element aus der COllection
+        .eq(0)
+        .css({
+            border: "2px solid green",
+            backgroundColor : "#aaaaff" // Alternativ dazu könnte man auch "background-color" schreiben
+        });
 }(jQuery));

2cb2543a9a85db3acb5d021a57e4700ccba71df2
Christoph Forster <christoph.forster@gmail.com>
2015-11-04 01:24:54 -0800

    3 Buttons - Button1: Toggle, Button2: Fading Animation, Button3 FadeToggle

--- js/bsp_13.js
+++ js/bsp_13.js
@@ -3,14 +3,39 @@
  */
 jQuery(function ($) {
     "use strict";
+
+    var $box1 = $("#box1");
     $("#box2").on("click", function (event) {
         var $target = $(event.target);
         if ($target.is("button")) {
             switch ($target.attr("id")) {
-                case "go1":
-                    console.log("hurra");
+                case "toggle":
+                    // duration, Animation Style (linear or swing), function after finishment
+                    //$box1.toggle(800, "swing", function () {
+                    //    $target.css("backgroundColor", "pink");
+                    //});
+
+                    $box1.toggle({
+                        duration: 1000,
+                        complete: function () {
+                            $target.text("Toggle");
+                        },
+                        start: function () {
+                            $target.text("running...");
+                        }
+                    });
                     break;
-                case "go2":
+                case "fadingAnimation":
+                    $box1
+                        .hide(500)
+                        .show(500)
+                        .fadeOut(500)
+                        .fadeIn(500)
+                        .fadeTo(1000, 0.3)
+                        .fadeTo(100, 1);
+                    break;
+                case "fadeToggle":
+                    $box1.fadeToggle();
                     break;
             }
         }

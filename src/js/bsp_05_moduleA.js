5ec981e869102d1c409e06b0c3662d07a7fc3552
Christoph Forster <christoph.forster@gmail.com>
2015-11-05 06:24:49 -0800

    moved sources to src dir add jsdoc tags in bsp_05_moduleA.js

--- src/js/bsp_05_moduleA.js
+++ src/js/bsp_05_moduleA.js
@@ -1,14 +1,28 @@
 /**
  * Created by devebiz on 11/2/15.
+ * @namespace SWAROVSKI
  */
 window.SWAROVSKI = window.SWAROVSKI || {};
+/**
+ *
+ * @module SWAROVSKI/shop
+ */
 window.SWAROVSKI.shop = (function () { // Closure
 
     "use strict";
 
+    /**
+     * private Variable für den Warenkorb
+     * @type {Array}
+     */
     var basket = []; // private
 
     return {
+        /**
+         * addItem
+         * @param item {String} Warenkorb
+         * @returns {window.SWAROVSKI.shop} Ware
+         */
         addItem: function (item) {
             basket.push(item);
 
